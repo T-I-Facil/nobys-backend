ac41916741c0fe8de5ca5cb9ff84d80ded52cea4
Shady Rajab <shadyrajaab@gmail.com>
2025-02-07 18:08:14 -0300

    refactor(Schedule)

--- src/models/schedule.py
+++ src/models/schedule.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel
 from typing import Optional
+from datetime import datetime
 
 class Schedule(BaseModel):
-    start_date: str
+    start_date: datetime
     schedule_time: int
     value: float
     specialty: Optional[str] = None
